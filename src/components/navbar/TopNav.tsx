54fbaf8dc756d581d675005ba4d619858666ce91
Samet Akbal <39596965+sametakbal@users.noreply.github.com>
2024-05-11 09:13:03 +0300

    navbar and layout

--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarContent} from "@nextui-org/react";
 import {GiFire} from "react-icons/gi";
 import Link from "next/link";
 import {Button} from "@nextui-org/button";
+import NavLink from "@/components/navbar/NavLink";
 
+const pages = [
+    {id: 1, label: 'Members', href: '/members'},
+    {id: 2, label: 'Lists', href: '/lists'},
+    {id: 3, label: 'Messages', href: '/messages'}
+];
 export default function TopNav() {
     return (
         <Navbar
@@ -13,7 +19,8 @@ export default function TopNav() {
                 item: [
                     'text-xl',
                     'text-white',
-                    'uppercase'
+                    'uppercase',
+                    'data-[active=true]:text-green-900'
                 ]
             }}
         >
@@ -25,14 +32,12 @@ export default function TopNav() {
                 </div>
             </NavbarBrand>
             <NavbarContent justify={'center'}>
-                <NavbarItem as={Link} href={'/members'}>Matches</NavbarItem>
-                <NavbarItem as={Link} href={'/list'}>Lists</NavbarItem>
-                <NavbarItem as={Link} href={'/messages'}>Messages</NavbarItem>
+                {pages.map(page => (<NavLink key={page.id} href={page.href} label={page.label}/>))}
             </NavbarContent>
 
             <NavbarContent justify={'end'}>
-                <Button variant='bordered' className='text-white'>{'login'}</Button>
-                <Button variant='bordered' className='text-white'>{'register'}</Button>
+                <Button variant='bordered' as={Link} href='/login' className='text-white'>{'login'}</Button>
+                <Button variant='bordered' as={Link} href='/register' className='text-white'>{'register'}</Button>
             </NavbarContent>
         </Navbar>
     )
